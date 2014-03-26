d21c298ca8d034666a21a73de50e094c1fa6da92
Nicole Bekkers <nicole@finalist.nl>
2014-03-26 19:20:52 +0100

    Rename todoApp to musicAlbumsApp

--- app/application.js
+++ app/application.js
@@ -1,6 +1,6 @@
-var todoApp = angular.module('musicAlbumsApp', []);
+var musicAlbumsApp = angular.module('musicAlbumsApp', []);
 
-todoApp.controller('AlbumsController', function($scope, $http) {
+musicAlbumsApp.controller('AlbumsController', function($scope, $http) {
   $http.get('data/albums.json').success(function(data) {
     $scope.albums = data
   });
