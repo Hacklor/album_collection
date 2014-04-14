c034a21eb138d7c3f07418101e280c9d95b75366
Nicole Bekkers <nicole@finalist.nl>
2014-04-14 16:14:18 +0200

    Add load path and default headers

--- lib/config/boot.rb
+++ lib/config/boot.rb
@@ -1,4 +1,11 @@
 require 'sinatra'
 require 'pry'
 
+$ROOT_PATH = "#{File.dirname(__FILE__)}/.."
+$LOAD_PATH << $ROOT_PATH
 Dir["./lib/api/*.rb"].each { |f| require f }
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  content_type :json
+end
